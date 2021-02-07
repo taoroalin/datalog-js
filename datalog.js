e28a0688d501202d109e5b5f042dba626c228846
Tao Lin <taoroalin@gmail.com>
2021-02-07 04:24:34 -0500

    RIP classes, hello functions

--- datalog.js
+++ datalog.js
@@ -1,3 +1,6 @@
+const maxEntityId = -1
+const manyAttributes = { "lines": true,":drawing/lines": true,":node/subpages": true,":vc/blocks": true,":edit/seen-by": true,":attrs/lookup": true,":node/windows": true,":node/sections": true,":harc/v": true,":block/refs": true,":harc/a": true,"children": true,":create/seen-by": true,":node/links": true,":query/results": true,":harc/e": true,":block/parents": true }
+
 const indexGet1 = (index,key1) => {
   if (index[key1] === undefined) {
     index[key1] = {}
@@ -18,156 +21,161 @@ const indexHas2 = (index,key1,key2) => {
   return index[key1] && index[key1][key2]
 }
 
-class DQ {
-  // negative numbers are entity ids
-  static maxEntityId = -1;
+// negative numbers are entity ids
 
-  constructor(graphName,many,nextEntityId = DQ.maxEntityId,eav,aev,vae) {
-    this.nextEntityId = nextEntityId
-    this.many = many
-    this.graphName = graphName
-    this.aev = {}
-    this.vae = {}
-    if (eav && aev && vae) {
-      this.eav = eav
-      this.aev = aev
-      this.vae = vae
-    } else if (eav) {
-      this.eav = eav
-      for (let ke in eav) {
-        const av = eav[ke]
-        for (let ka in av) {
-          if (this.aev[ka] === undefined) {
-            this.aev[ka] = {}
+const roamJsonToDatabase = (graphName,roamJson) => {
+  const result = {
+    nextEntityId: maxEntityId,
+    graphName: graphName,
+    eav: {},
+    aev: {},
+    vae: {}
+  }
+  for (let page of roamJson) {
+    databasePush(result,page)
+  }
+  return result
+}
+
+const eavToDatabase = (graphName,eav) => {
+  const result = {
+    nextEntityId: maxEntityId,
+    graphName: graphName,
+    eav: eav,
+    aev: {},
+    vae: {}
+  }
+  for (let ke in eav) {
+    const av = eav[ke]
+    for (let ka in av) {
+      if (result.aev[ka] === undefined) {
+        result.aev[ka] = {}
+      }
+      const v = av[ka]
+      result.aev[ka][ke] = v
+      if (typeof v !== "object") {
+        if (result.vae[v] === undefined) {
+          result.vae[v] = {}
+        }
+        const ae = result.vae[v]
+        if (ae[ka] === undefined)
+          ae[ka] = []
+        ae[ka].push(ke)
+      } else if (v instanceof Array) {
+        for (let sv of v) {
+          if (result.vae[sv] === undefined) {
+            result.vae[sv] = {}
           }
-          const v = av[ka]
-          this.aev[ka][ke] = v
-          if (typeof v !== "object") {
-            if (this.vae[v] === undefined) {
-              this.vae[v] = {}
-            }
-            const ae = this.vae[v]
-            if (ae[ka] === undefined)
-              ae[ka] = []
-            ae[ka].push(ke)
-          } else if (v instanceof Array) {
-            for (let sv of v) {
-              if (this.vae[sv] === undefined) {
-                this.vae[sv] = {}
-              }
-              const ae = this.vae[sv]
-              if (ae[ka] === undefined)
-                ae[ka] = []
-              ae[ka].push(ke)
-            }
-          }
+          const ae = result.vae[sv]
+          if (ae[ka] === undefined)
+            ae[ka] = []
+          ae[ka].push(ke)
         }
       }
-    } else {
-      this.eav = {}
     }
   }
+  return result
+}
 
-  newEntity() {
-    this.nextEntityId -= 1
-    return this.nextEntityId + 1
+const databaseNewEntity = (database) => {
+  database.nextEntityId -= 1
+  return database.nextEntityId + 1
+}
+
+const databaseAddDatom = (database,entity,attribute,value) => {
+  database.nextEntityId = Math.min(database.nextEntityId,entity + 1)
+  indexGet2(database.eav,entity,attribute).push(value)
+  indexGet2(database.aev,attribute,entity).push(value)
+
+  if (typeof value !== "object") {
+    indexGet2(database.vae,value,attribute).push(entity)
   }
+}
 
-  addDatom(entity,attribute,value) {
-    this.nextEntityId = Math.min(this.nextEntityId,entity + 1)
-    indexGet2(this.eav,entity,attribute).push(value)
-    indexGet2(this.aev,attribute,entity).push(value)
+const databaseSetDatom = (database,entity,attribute,value) => {
+  database.nextEntityId = Math.min(database.nextEntityId,entity + 1)
+  indexGet1(database.eav,entity)[attribute] = value
+  indexGet1(database.aev,attribute)[entity] = value
 
-    if (typeof value !== "object") {
-      indexGet2(this.vae,value,attribute).push(entity)
+  if (typeof value !== "object") {
+    indexGet2(database.vae,value,attribute).push(entity)
+  }
+}
+
+const databasePull = (database,entityId,includeId = false) => {
+  // Keep track of seen entities to capture recursive data structures
+  const entityIdToObj = {}
+  const pull = (entityId) => {
+    const result = {}
+    if (includeId) result.entityId = entityId
+    entityIdToObj[entityId] = result
+    const entity = indexGet1(database.eav,entityId)
+    for (let attribute in entity) {
+      const value = entity[attribute]
+      if (attribute === ":block/refs") {
+        result[attribute] = value.map(uid => ({ ":block/uid": uid }))
+      } else if (attribute === "refs") {
+        result[attribute] = value.map(uid => ({ "uid": uid }))
+      } else if (attribute === ":create/user" || attribute === ":edit/user") {
+        result[attribute] = { ":user/uid": value }
+      } else if (database.many[attribute]) {
+        result[attribute] = []
+        for (let v of value) {
+          if (typeof v === "number" && v <= DQ.maxEntityId) {
+            if (entityIdToObj[v])
+              result[attribute].push(entityIdToObj[v])
+            else result[attribute].push(pull(v))
+          } else result[attribute].push(v)
+        }
+      } else {
+        if (typeof value === "number" && value <= DQ.maxEntityId) {
+          if (entityIdToObj[value])
+            result[attribute] = entityIdToObj[value]
+          else result[attribute] = pull(value)
+        } else result[attribute] = value
+      }
     }
+    return result
   }
+  return pull(entityId)
+}
 
-  setDatom(entity,attribute,value) {
-    this.nextEntityId = Math.min(this.nextEntityId,entity + 1)
-    indexGet1(this.eav,entity)[attribute] = value
-    indexGet1(this.aev,attribute)[entity] = value
-
-    if (typeof value !== "object") {
-      indexGet2(this.vae,value,attribute).push(entity)
-    }
-  }
-
-  pull(entityId,includeId = false) {
-    // Keep track of seen entities to capture recursive data structures
-    const entityIdToObj = {}
-    const pull = (entityId) => {
-      const result = {}
-      if (includeId) result.entityId = entityId
-      entityIdToObj[entityId] = result
-      const entity = indexGet1(this.eav,entityId)
-      for (let attribute in entity) {
-        const value = entity[attribute]
+const databasePush = (database,obj,objId) => {
+  if (objId === undefined) objId = databaseNewEntity(database)
+  const push = (obj,objId) => {
+    for (let attribute in obj) {
+      const value = obj[attribute]
+      if (typeof value !== "object") {
+        databaseSetDatom(database,objId,attribute,value)
+      } else if (value instanceof Array) {
         if (attribute === ":block/refs") {
-          result[attribute] = value.map(uid => ({ ":block/uid": uid }))
+          for (let x of value) {
+            databaseAddDatom(database,objId,attribute,x[":block/uid"])
+          }
         } else if (attribute === "refs") {
-          result[attribute] = value.map(uid => ({ "uid": uid }))
-        } else if (attribute === ":create/user" || attribute === ":edit/user") {
-          result[attribute] = { ":user/uid": value }
-        } else if (this.many[attribute]) {
-          result[attribute] = []
-          for (let v of value) {
-            if (typeof v === "number" && v <= DQ.maxEntityId) {
-              if (entityIdToObj[v])
-                result[attribute].push(entityIdToObj[v])
-              else result[attribute].push(pull(v))
-            } else result[attribute].push(v)
+          for (let x of value) {
+            databaseAddDatom(database,objId,attribute,x["uid"])
           }
         } else {
-          if (typeof value === "number" && value <= DQ.maxEntityId) {
-            if (entityIdToObj[value])
-              result[attribute] = entityIdToObj[value]
-            else result[attribute] = pull(value)
-          } else result[attribute] = value
-        }
-      }
-      return result
-    }
-    return pull(entityId)
-  }
-
-  push(obj,objId) {
-    if (objId === undefined) objId = this.newEntity()
-    const push = (obj,objId) => {
-      for (let attribute in obj) {
-        const value = obj[attribute]
-        if (typeof value !== "object") {
-          this.setDatom(objId,attribute,value)
-        } else if (value instanceof Array) {
-          if (attribute === ":block/refs") {
-            for (let x of value) {
-              this.addDatom(objId,attribute,x[":block/uid"])
-            }
-          } else if (attribute === "refs") {
-            for (let x of value) {
-              this.addDatom(objId,attribute,x["uid"])
-            }
-          } else {
-            for (let setElement of value) {
-              if (typeof setElement !== "object" || setElement instanceof Array) {
-                this.addDatom(objId,attribute,setElement)
-              } else {
-                let elId = this.newEntity()
-                this.addDatom(objId,attribute,elId)
-                push(setElement,elId)
-              }
+          for (let setElement of value) {
+            if (typeof setElement !== "object" || setElement instanceof Array) {
+              databaseAddDatom(database,objId,attribute,setElement)
+            } else {
+              let elId = databaseNewEntity(database)
+              databaseAddDatom(database,objId,attribute,elId)
+              push(setElement,elId)
             }
           }
-        } else if (attribute === ":create/user" || attribute === ":edit/user") {
-          this.setDatom(objId,attribute,value[":user/uid"])
-        } else {
-          let valId = this.newEntity()
-          this.setDatom(objId,attribute,valId)
-          push(value,valId)
         }
+      } else if (attribute === ":create/user" || attribute === ":edit/user") {
+        databaseSetDatom(database,objId,attribute,value[":user/uid"])
+      } else {
+        let valId = databaseNewEntity(database)
+        databaseSetDatom(database,objId,attribute,valId)
+        push(value,valId)
       }
     }
-    push(obj,objId)
-    return objId
   }
+  push(obj,objId)
+  return objId
 }
